ffa2a0f5e65e1708a9cf76c780815760f6b2ea4a
Mulalic97 <mustafa.mulalic@yahoo.com>
2023-12-01 00:34:10 +0100

    feat: add cannot register with existing mail test

--- tests/cannot-register-with-existing-mail.spec.ts
+++ tests/cannot-register-with-existing-mail.spec.ts
@@ -1,31 +1,33 @@
 import { test } from '../utils/test'
 import { RegisterPage } from '../page/register.page'
 import { expect } from '@playwright/test'
+
 import { generateRandomString } from '../helpers/helper'
-import { CampaignsPage } from '../page/campaigns.page'
 import { EditUserPage } from '../page/edit-user.page'
+import { LoginPage } from '../page/login.page'
 
 const { name, email, password } = generateRandomString()
 
-test.describe('Logged in user successfully adds campaigns', () => {
-  test('User adds campaign', async ({ page, constants }) => {
+test.describe('Existing email register', () => {
+  test('User registers', async ({ page, constants }) => {
     const registerPage = new RegisterPage(page, constants)
-    const campaignsPage = new CampaignsPage(page, constants)
     const editUserPage = new EditUserPage(page, constants)
+    const loginPage = new LoginPage(page, constants)
 
     await registerPage.register(name, email, password)
-    const successMessage = page.locator('#flash_notice')
-    await expect(successMessage).toBeVisible()
-    await expect(successMessage).toContainText(
+    const successRegister = page.locator('#flash_notice')
+    await expect(successRegister).toBeVisible()
+    await expect(successRegister).toContainText(
       'Welcome! You have signed up successfully.',
     )
+    const signOutButton = await page.$('a[href="/users/sign_out"]')
+    await signOutButton.click()
 
-    await campaignsPage.verifyAddingCampaign(name)
-    await expect(successMessage).toContainText(
-      'Campaign was successfully created.',
-    )
-    const campaignElement = page.locator(`(//td[contains(text(),${name})])[1]`)
-    await expect(campaignElement).toBeVisible()
+    await registerPage.register(name, email, password)
+    const errorMessage = await page.locator('#error_explanation')
+    await expect(errorMessage).toBeVisible()
+
+    await loginPage.login(email, password)
 
     await editUserPage.cancelMyAccount()
     const successAccountCancel = page.locator('#flash_notice')
