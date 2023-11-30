8e547839d057d5a128df9891da007a2b8f9d7df2
Mulalic97 <mustafa.mulalic@yahoo.com>
2023-11-30 09:15:33 +0100

    feat: create edit page and cancel my account

---
+++ page/edit-user.page.ts
@@ -0,0 +1,39 @@
+import {Locator, Page} from "@playwright/test";
+import {Constants} from "../utils/test";
+
+
+export class EditUserPage {
+    private readonly page: Page;
+    private readonly constants;
+
+// Element Locators
+    private readonly cancelMyAccountButton : Locator;
+
+    constructor(page: Page, constants: Constants) {
+
+        this.page = page;
+        this.constants = constants;
+
+        this.cancelMyAccountButton= page.getByRole('button', {name:'Cancel my account'})
+    }
+
+    async goto() {
+        await this.page.goto(`${this.constants.webClientURL}/users/edit`);
+    }
+
+    async clickCancelAccount(){
+        await this.cancelMyAccountButton.click();
+    }
+
+    async handleDialog(){
+
+       await this.page.on('dialog', dialog => dialog.accept());
+       await this.clickCancelAccount();
+
+    }
+async cancelMyAccount(){
+    await this.goto();
+    await this.clickCancelAccount();
+    await this.handleDialog();
+}
+}
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,15 +1,22 @@
 import {RegisterPage} from "../page/register.page";
+import {EditUserPage} from "../page/edit-user.page";
 import {test} from "../utils/test";
 import {expect} from "@playwright/test";
 
 test.describe('Register', () => {
     test('User successfully Registered', async ({ page, constants }) => {
         const registerPage = new RegisterPage(page, constants);
+        const editUserPage = new EditUserPage(page,constants)
 
         await registerPage.register();
-        const successElement = page.locator('#flash_notice');
-        await expect(successElement).toBeVisible();
-        await expect(successElement).toContainText('Welcome! You have signed up successfully.')
+        const successRegister = page.locator('#flash_notice');
+        await expect(successRegister).toBeVisible();
+        await expect(successRegister).toContainText('Welcome! You have signed up successfully.');
+
+        await editUserPage.cancelMyAccount();
+        const successAccountCancel = page.locator('#flash_notice')
+        await expect(successAccountCancel).toContainText('Bye! Your account has been successfully cancelled. We hope to see you again soon.');
+
 
     });
 });
